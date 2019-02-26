78b877a27dbac6e0e5d515769cdc459e249a1062
Charlotte Liu <liu.charlotte@gmail.com>
2019-02-26 20:40:47 +0800

    change setting

--- app/resources/js/router/index-route_app1.js
+++ app/resources/js/router/index-route_app1.js
@@ -13,9 +13,10 @@ config(['$routeProvider', function($routeProvider) {
 
 ClassicModelsApp.constant('ClassicMdConfig', {
     appName: 'ClassicModelsApp',
-    appVersion: 1.1,
+    appVersion: 2.1,
     apiUrl: 'http://www.google.com?api',
     // dataApiUrl: 'http://jbossews-bluedream.rhcloud.com/' // For OpenShift v2
     // dataApiUrl: 'http://classicmodel-augjs1-mytestprj1.193b.starter-ca-central-1.openshiftapps.com/classicmodel-ap1/'    // For OpenShift v3
-	dataApiUrl: 'http://classicmodel-augjs1-mytestprj1.a3c1.starter-us-west-1.openshiftapps.com/classicmodel-ap1/'    // For OpenShift v3
+	//dataApiUrl: 'http://classicmodel-augjs1-mytestprj1.a3c1.starter-us-west-1.openshiftapps.com/classicmodel-ap1/'    // For OpenShift v3
+	dataApiUrl: 'http://classicwebap1-env.cay7budsje.us-west-2.elasticbeanstalk.com/'    // For AWS EB	
       });
